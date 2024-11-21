d8c39acbb461ef83271f84900afe8943a8376f2c
Alex <le86ksa@yandex.ru>
2024-11-21 18:33:09 +0300

    04_events

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,56 +1,92 @@
 package ru.netology.nmedia
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
+import android.util.Log
 import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 
 class MainActivity : AppCompatActivity() {
+
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println(resources.displayMetrics.heightPixels) // 1794
-        println(resources.displayMetrics.widthPixels) // 1080
-        println(resources.displayMetrics.densityDpi) // 420
-        println(resources.displayMetrics.density) // 2.625
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //ик в нутри рпиложения
         val avatar: ImageView = findViewById(R.id.avatar)
         avatar.setImageResource(R.drawable.ic_netology_48dp)
 
-        val author: TextView = findViewById(R.id.author)
-        author.setText(R.string.author_text)
 
-        val published: TextView = findViewById(R.id.published)
-        published.setText(R.string.published_text)
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb ",
+            likedByMy = false,
+            likes = 999,
+            shares = 999,
+        )
 
-        val content: TextView = findViewById(R.id.content)
-        content.setText(R.string.content_text)
+        // Инициализация значений для отображения
+        binding.likeCount.text = formatCount(post.likes)
+        binding.shareCount.text = formatCount(post.shares)
 
-        val like: ImageButton = findViewById(R.id.like)
-        var likeCounter = 998
-        val likeText: TextView = findViewById(R.id.like_count)
-        likeText.setText("$likeCounter")
-        like.setOnClickListener {
-            if (likeCounter >= 1000) {
-                likeText.setText("+1k")
-            } else {
-                likeText.setText("${likeCounter++}")
-            }
+        binding.content.text = post.content
+        binding.published.text = post.published
+        binding.author.text = post.author
+
+        // Клик на like
+        binding.like.setOnClickListener {
+            post.likedByMy = !post.likedByMy
+            post.likes = if (post.likedByMy) post.likes + 1 else post.likes - 1
+            binding.likeCount.text = formatCount(post.likes)
+                  updateLikeImage(binding, post)
+            //        Log.d("MainActivity", "Клик на like")
+
         }
 
-        val share: ImageButton = findViewById(R.id.share)
-        var shareCounter = 350
-        val shareText: TextView = findViewById(R.id.share_count)
-        shareText.setText("$shareCounter")
-        share.setOnClickListener {
-            shareText.setText("${shareCounter++}")
+        // Клик на share
+        binding.share.setOnClickListener {
+            post.shares++
+            binding.shareCount.text = formatCount(post.shares)
+            Log.d("MainActivity", "Клик на share")
+        }
+
+        // Обработчик на корневой элемент (binding.root) - выводит LOG если нажат любой другой элемент
+        //    binding.root.setOnClickListener {
+        //       Log.d("MainActivity", "Клик на root")
+        //   }
+
+        //   binding.avatar.setOnClickListener {
+        //         Log.d("MainActivity", "Клик на аватар")
+        //    }
+
+        updateLikeImage(binding, post) // Обновляем картинку лайка при запуске
+    }
+
+
+    private fun updateLikeImage(binding: ActivityMainBinding, post: Post) {
+        binding.like.setImageResource(
+            if (post.likedByMy) {
+                R.drawable.ic_baseline_favorite_24
+            } else {
+                R.drawable.ic_baseline_favorite_border_24
+            }
+        )
+    }
+
+
+    private fun formatCount(count: Int): String {
+        return when {
+            count < 1000 -> count.toString()
+            count < 10000 -> (count / 1000).toString() + "K"
+            count < 1000000 -> (count / 1000).toString() + "K"
+            else -> (count / 1000000).toString() + "M"
         }
     }
 }
-
-
-//        println(R.string.hello) // число
-//        println(getString(R.string.hello)) // "Привет, Мир!" или "Hello World!"
